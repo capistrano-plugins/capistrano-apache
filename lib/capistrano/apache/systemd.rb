4dd1038140c4bd05fab5ef6abbe664f72587e227
Nathan Pierce <connarpierce@gmail.com>
2018-01-27 23:04:21 -0500

    v3.0.0: New structure for using systemd and sysv_upstart

--- lib/capistrano/apache/systemd.rb
+++ lib/capistrano/apache/systemd.rb
@@ -1,2 +1,2 @@
-load File.expand_path('apache.rake', __FILE__) # Load configuration options
-load File.expand_path('tasks/systemd.rake', __FILE__)
+load File.expand_path('../apache.rake', __FILE__) # Load configuration options
+load File.expand_path('../tasks/systemd.rake', __FILE__)
--- lib/capistrano/apache/sysv_upstart.rb
+++ lib/capistrano/apache/sysv_upstart.rb
@@ -1,2 +1,2 @@
-load File.expand_path('apache.rake', __FILE__) # Load configuration options
-load File.expand_path('tasks/sysv_upstart.rake', __FILE__)
+load File.expand_path('../apache.rake', __FILE__) # Load configuration options
+load File.expand_path('../tasks/sysv_upstart.rake', __FILE__)
